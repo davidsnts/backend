58991a9ae385df636108ea1d77c82649e5f26119
David <seuemail@exemplo.com>
2025-12-05 14:09:47 -0300

    feat: add middleware rota admin

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -1,9 +1,6 @@
 import { Request, Response } from "express";
 import { DetailUserService } from "../../services/user/DetailUserService";
 
-
-
-
 class DetailUserController {
     async handle(req: Request, res: Response) {
         
--- src/routes.ts
+++ src/routes.ts
@@ -5,12 +5,14 @@ import { validateSchema } from "./middlewares/validateSchema";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { isAdmin } from "./middlewares/isAdmin";
 
 const router = Router();
 
 router.post('/users', validateSchema(createUserSchema), new CreateUserController().handle);
 router.post("/session", validateSchema(authUserSchema), new AuthUserController().handle);
-router.get("/me", isAuthenticated ,new DetailUserController().handle)
-
+router.get("/me", isAuthenticated, new DetailUserController().handle)
+router.post("/category", isAuthenticated, isAdmin, new CreateCategoryController().handle)
 
 export { router };
