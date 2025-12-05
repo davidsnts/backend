0c3354ab9228527692b2f0c0388ecfd1d84fcc9b
David <seuemail@exemplo.com>
2025-12-05 15:10:31 -0300

    feat: add schema de categoria

--- src/routes.ts
+++ src/routes.ts
@@ -7,12 +7,13 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { isAdmin } from "./middlewares/isAdmin";
+import { createCategorySchema } from "./schemas/categorySchema";
 
 const router = Router();
 
 router.post('/users', validateSchema(createUserSchema), new CreateUserController().handle);
 router.post("/session", validateSchema(authUserSchema), new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle)
-router.post("/category", isAuthenticated, isAdmin, new CreateCategoryController().handle)
+router.post("/category", isAuthenticated, isAdmin,validateSchema(createCategorySchema), new CreateCategoryController().handle)
 
 export { router };
